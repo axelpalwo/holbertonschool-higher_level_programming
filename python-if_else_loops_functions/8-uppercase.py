b2390b34399569ed2792c4a982f150088dacab24
Axel <Axel.palombo.ap@gmail.com>
2024-09-04 12:21:27 -0300

    eigth exercise fixed

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+    
 def uppercase(str):
     i = 0
     while i < len(str):
-        if i < len(str) - 1:
-            print("{}".format(chr(ord(str[i]) - 32)), end='')
+        if islower((str[i])):
+            if i < len(str) - 1:
+                print("{}".format(chr(ord(str[i]) - 32)), end='')
+            else:
+                print("{}".format(chr(ord(str[i]) - 32)))
         else:
-            print("{}".format(chr(ord(str[i]) - 32)))
+            print("{}".format(str[i]), end='')
         i += 1
