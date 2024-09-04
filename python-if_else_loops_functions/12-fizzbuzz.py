72e3c3973397632b23b4b09a3fc6da0ab9c94021
Axel <Axel.palombo.ap@gmail.com>
2024-09-04 12:57:04 -0300

    last exercise

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+i = 1
+while i <= 100:
+    if i % 5 == 0 and 5 % 3 == 0:
+        print('FizzBuzz', end=' ')
+    elif i % 5 == 0:
+        print('Buzz', end=' ')
+    elif i % 3 == 0:
+        print('Fizz', end=' ')
+    else:
+        print(f'{i}', end=' ')
+    i += 1
+print()
