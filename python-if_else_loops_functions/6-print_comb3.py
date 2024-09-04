17742f298be16ca90b11a5f3a1ee4dd5fc05dd31
Axel <Axel.palombo.ap@gmail.com>
2024-09-04 12:07:15 -0300

    seventh exercise

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     for v in range(1):
-        if i % 10 == i / 10 or i % 10 > i / 10:
+        if (i % 10 == i / 10 or i % 10 > i / 10) and i != 0:
             if i < 80:
                 print("{:02}".format(i), end=", ")
             else:
