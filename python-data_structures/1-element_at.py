c5c6cd69fa2dd4920198b8867349bd481065558b
Axel <Axel.palombo.ap@gmail.com>
2024-09-10 13:40:36 +0000

    first exercise

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     if idx > len(my_list):
         return None
     elif idx >= 0:
-        print("{:d}".format(my_list(idx)))
+        print("{:d}".format(my_list[idx]))
     else:
         return None
