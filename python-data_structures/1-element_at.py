1a213237705a0ac30d49b5975f2d58a9d8dc180f
Axel <Axel.palombo.ap@gmail.com>
2024-09-10 13:57:07 +0000

    fixed return

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list) - 1:
+    if idx > len(my_list) - 1 or idx < 0:
         return None
     elif idx >= 0:
         return (f"Element at index {idx} is {my_list[idx]}")
-    else:
-        return None
+
