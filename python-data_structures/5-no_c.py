dc49b1d210201417278625e4da57d259ea5895e5
Axel <Axel.palombo.ap@gmail.com>
2024-09-10 17:50:49 +0000

    fifth exercise

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     index = 0
-    new_string = []
+    new_string = ""
     while index < len(my_string):
         if my_string[index] != 'c' and my_string[index] != 'C':
-            new_string.append(my_string[index])
+            new_string += my_string[index]
         index += 1
     return new_string
