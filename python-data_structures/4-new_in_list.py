90ca9c99c76162900143286b297af4fa1fe7227b
Axel <Axel.palombo.ap@gmail.com>
2024-09-10 17:42:51 +0000

    fourth exercise

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,4 +3,5 @@ def new_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list) - 1:
         new_list = my_list.copy()
         new_list[idx] = element
+        return new_list
     return my_list
