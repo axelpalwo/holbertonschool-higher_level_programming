d3ad01e690ede1321015c63bf24c2e3c9e5f340a
Axel <Axel.palombo.ap@gmail.com>
2024-09-11 13:12:59 +0000

    ninth exercise

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     max_num = None
     i = 0
     while i < len(my_list):
-        if max_num == None:
+        if max_num is None:
             max_num = my_list[i]
         if max_num < my_list[i]:
             max_num = my_list[i]
