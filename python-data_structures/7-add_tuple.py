f7a504d6b4e6d26782e53dbd4bc374594a9fdd59
Axel <Axel.palombo.ap@gmail.com>
2024-09-11 13:01:34 +0000

    seventh exercise

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a_value = 0
+    b_value = 0
+
+    if len(tuple_a) > 0:
+        a_value += tuple_a[0]
+    if len(tuple_b) > 0:
+        a_value += tuple_b[0]
+
+    if len(tuple_a) == 2:
+        b_value += tuple_a[1]
+    if len(tuple_b) == 2:
+        b_value += tuple_b[1]
+
+    return (a_value, b_value)
