dcce6ef48466884dec1aa09ef182699c7f634c82
Axel <Axel.palombo.ap@gmail.com>
2024-09-12 16:47:01 +0000

    third exercise

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,8 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-        print("Inside result: {}".format(result))
     except:
+        result = None
+    finally:
         print("Inside result: {}".format(result))
     return result
