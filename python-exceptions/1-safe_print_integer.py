61c66c612146e0fe4863b848397aa5448eef9ab1
Axel <Axel.palombo.ap@gmail.com>
2024-09-12 16:31:04 +0000

    first exercise

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,18 +6,3 @@ def safe_print_integer(value):
     except:
         print("{} is not an integer".format(value))
         return True
-
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
