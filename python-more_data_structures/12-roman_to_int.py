1084fa1a6d1caa0decb1eb01cbe3ab3b07e0c352
Axel <Axel.palombo.ap@gmail.com>
2024-09-12 16:03:00 +0000

    last exercise

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    resultado = 0
+    if roman_string is not None:
+        i = 0
+        while i < len(roman_string):
+            if i + 1 != len(roman_string):
+                if roman[roman_string[i]] < roman[roman_string[i + 1]]:
+                    subtotal = roman[roman_string[i + 1]] - roman[roman_string[i]]
+                    resultado += subtotal
+                    i += 2
+                else:
+                    resultado += roman[roman_string[i]]
+                    i += 1
+            else:
+                resultado += roman[roman_string[i]]
+                i += 1
+    return resultado
