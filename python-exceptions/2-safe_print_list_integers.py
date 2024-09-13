94a23a99a0c870f8e6db89bef7daeb7f90ed4125
Axel <Axel.palombo.ap@gmail.com>
2024-09-13 21:10:00 +0000

    fixed 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,6 +2,8 @@
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     counter = 0
+    if x > len(my_list):
+        x = len(my_list)
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end='')
