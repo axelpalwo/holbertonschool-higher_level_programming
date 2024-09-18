04dfb20573e28c7ad77aab8e5aa0fb46f40e2464
Axel <Axel.palombo.ap@gmail.com>
2024-09-18 13:02:13 +0000

    3 done

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,19 +2,16 @@
 """
 Este módulo define una clase llamada Square.
 
-La clase Square está diseñada para representar un cuadrado
-y tiene un atributo privado
-llamado __size que almacenará el tamaño del lado del cuadrado.
+La clase Square está diseñada para representar un cuadrado y contiene un atributo
+privado llamado __size, que almacena el tamaño del lado del cuadrado.
 """
-
 
 class Square:
     """
     Clase Square que representa un cuadrado.
 
-    Esta clase contiene un atributo privado llamado __size,
-    que se utiliza para almacenar
-    el tamaño de un lado del cuadrado.
+    Atributos:
+        __size (int): El tamaño de un lado del cuadrado, debe ser un entero >= 0.
     """
 
     def __init__(self, size=0):
@@ -22,14 +19,23 @@ class Square:
         Inicializa un nuevo objeto de la clase Square.
 
         Parámetros:
-        size (int): El tamaño de un lado del cuadrado (opcional).
-        Si no se proporciona, será None.
+        size (int): El tamaño de un lado del cuadrado (por defecto es 0).
+
+        Raises:
+        TypeError: Si size no es un entero.
+        ValueError: Si size es menor que 0.
         """
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
-        # Atributo privado que almacena el tamaño del cuadrado
-        def area(self):
-            return size * size
+            raise ValueError("size must be >= 0")
+        self.__size = size  # Atributo privado que almacena el tamaño del cuadrado
+
+    def area(self):
+        """
+        Calcula y devuelve el área del cuadrado.
+
+        Returns:
+        int: El área del cuadrado, que es size al cuadrado.
+        """
+        return self.__size ** 2
