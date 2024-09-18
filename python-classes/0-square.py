a88d8ae2301af851f7b8435628f0fc92250bad02
Axel <Axel.palombo.ap@gmail.com>
2024-09-18 12:27:12 +0000

    0 fixed

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,16 +2,16 @@
 """
 Este módulo define una clase llamada Square.
 
-La clase Square es actualmente una implementación vacía, que sirve como base para
+La clase Square es actualmente una implementación vacía,
+que sirve como base para
 futuras mejoras o expansiones relacionadas con cuadrados.
 """
-
 class Square:
     """
     Clase Square que representa un cuadrado.
 
     Actualmente, esta clase no contiene atributos ni métodos. Se puede expandir
-    en el futuro para incluir características como la longitud de los lados, el área,
-    el perímetro, etc.
+    en el futuro para incluir características como la longitud
+    de los lados, el área, el perímetro, etc.
     """
     pass
