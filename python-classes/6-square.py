f735f17c020ca222682a0f34624147f783bdd006
Axel <Axel.palombo.ap@gmail.com>
2024-09-18 13:52:03 +0000

    6 done

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -68,10 +68,10 @@ class Square:
         TypeError: Si value no es una tupla de dos enteros positivos.
         """
         if (not isinstance(value, tuple) or
-        len(value) != 2 or
-        not all(isinstance(x, int) for x in value) or
-        not all(x >= 0 for x in value)):
-            raise TypeError('position must be a tuple of 2 positive integers')
+            len(value) != 2 or
+            not all(isinstance(x, int) for x in value) or
+            not all(x >= 0 for x in value)):
+                raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
     @property
