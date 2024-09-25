77f3fcb193203c09874b50f07e56f5a555c88e14
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-09-25 11:56:02 -0300

    fixed pep

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,6 +4,8 @@ is_same_class its a function that compares classes
 Returns: True in positive case
 '''
 def is_same_class(obj, a_class):
+
+    
     '''
     Returns true or false if an obj is instance of a class
     '''
