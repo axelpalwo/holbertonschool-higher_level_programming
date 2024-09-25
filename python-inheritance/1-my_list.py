72bc7851388cd5549b596cbb43173cc4c08d641f
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-09-25 12:38:27 -0300

    finally fixed doc test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,5 +9,8 @@ class MyList(list):
     '''
     This method prints the list sorted
     '''
+
     def print_sorted(self):
-        print(sorted(self))
+        """Imprime una lista ordenada."""
+        sorted_list = sorted(self)
+        return sorted_list   # Return the sorted list
