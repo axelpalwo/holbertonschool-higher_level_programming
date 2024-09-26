c919ea1da3a018a6868a6f7d39f4d693c787f9c1
AxelPalWO <140984664+AxelPalWO@users.noreply.github.com>
2024-09-26 14:38:47 -0300

    6 task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Base Geometry Class
+'''
+
+
+class BaseGeometry:
+    '''
+    Base Geometry object
+    '''
+    def area(self):
+        raise Exception('area() is not implemented')
