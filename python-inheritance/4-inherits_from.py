dd259c6d5a640809b6f869029a05e6e7f5ff4267
AxelPalWO <140984664+AxelPalWO@users.noreply.github.com>
2024-09-27 13:40:14 -0300

    fixed 4 pep8

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     '''
     Returns true or false if an obj is instance of a class
     '''
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
