0158f8dc1ee313fd38d5bab9e53274466bef8ed0
AxelPalWO <140984664+AxelPalWO@users.noreply.github.com>
2024-09-30 12:21:42 -0300

    funct doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,10 @@ This function opens a file by filename
 
 
 def read_file(filename=""):
+    """
+    with statements closes the file after reading
+    open(filename) opens the file
+    read() reads the contents of the file
+    """
     with open(filename) as f:
         print(f.read())
