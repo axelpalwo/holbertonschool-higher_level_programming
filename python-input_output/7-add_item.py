d2906a1a3339ec79ebf0370248c99fb774a3e69b
AxelPalWO <140984664+AxelPalWO@users.noreply.github.com>
2024-09-30 13:42:00 -0300

    added some doc

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,21 @@
+#!/usr/bin/python3
+"""
+We import sys to use sys argv
+"""
+
+
 import sys
+"""
+Save to Json function
+"""
+
+
 saveToJson = __import__('5-save_to_json_file').save_to_json_file
+"""
+Load from Json function
+"""
+
+
 loadFromJson = __import__('6-load_from_json_file').load_from_json_file
 
 filename = 'add_item.json'
