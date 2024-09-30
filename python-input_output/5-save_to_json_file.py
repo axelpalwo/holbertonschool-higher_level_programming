4de6b8f3e4ebf1f08338578aa1ac7e00bff038e3
AxelPalWO <140984664+AxelPalWO@users.noreply.github.com>
2024-09-30 12:55:51 -0300

    funct doc

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,6 +12,11 @@ and then saves it in a file
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    open in mode 'a' to append
+    write() to write in the file
+    json.dumps to convert into string the obj
+    """
     with open(filename, 'a', encoding='utf-8') as f:
         f.write(json.dumps(my_obj))
         return
