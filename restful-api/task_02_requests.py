f71fa36c617948c188a3a374693e2d24e71fcdaf
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2024-10-10 13:23:52 -0300

    fixed url

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,7 +3,7 @@ import requests # type: ignore
 import csv
 
 def fetch_and_print_posts():
-    res = requests.get('https://jsonplaceholder.typicode.com/todos')
+    res = requests.get('https://jsonplaceholder.typicode.com/posts')
     print(f"Status Code: {res.status_code}")
     if res.status_code == 200:
         res = res.json()
@@ -13,14 +13,14 @@ def fetch_and_print_posts():
         print("Error with Fetch")
 
 def fetch_and_save_posts():
-    res = requests.get('https://jsonplaceholder.typicode.com/todos')
+    res = requests.get('https://jsonplaceholder.typicode.com/posts')
     if res.status_code == 200:
         res = res.json()
         with open('posts.csv', mode='w', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
             for data in res:
-                print({'id': data['id'], 'title': data['title'], 'body': data})
-                writer.writerow({'id': data['id'], 'title': data['title'], 'body': data})
+                print({'id': data['id'], 'title': data['title'], 'body': data['body']})
+                writer.writerow({'id': data['id'], 'title': data['title'], 'body': data['body']})
     else:
         print("Error with Fetch")
 
