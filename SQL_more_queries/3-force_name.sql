3f6dfcda722de217867449522984cbd1faf5a8f1
Axel <Axel.palombo.ap@gmail.com>
2024-11-01 19:53:08 +0000

    3rd exercise

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Create a table
+-- name of column - type of data - Primary/Foreign Key (Optional) - constrictions (Optional)
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
