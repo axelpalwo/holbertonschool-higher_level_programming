b05b5e96d936cc188e941c00731d8a3aca3a82b7
Axel <Axel.palombo.ap@gmail.com>
2024-11-01 20:41:56 +0000

    10th exercise

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- Selects Title from tv_shows and genre_id from tv_show_genres
 -- Then Joins tv_show_genres looking through tv_show_genres with tv_show_id
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
