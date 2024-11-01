34e5316f212f9ec0962a5c7b0f8bdea26149b5b6
Axel <Axel.palombo.ap@gmail.com>
2024-11-01 19:37:22 +0000

    second exercise

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- Create Database and User
+-- Grants priv to User on Database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
