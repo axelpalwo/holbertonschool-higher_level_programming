9cb901ef9db1909e895460b64b5cae83f933fc8a
Axel <Axel.palombo.ap@gmail.com>
2024-11-01 20:02:42 +0000

    4th exercise

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Create a table
+-- name of column - type of data - DEFAULT (Value) - Primary/Foreign Key (Optional) - constrictions (Optional)
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT DEFAULT 1
+    name VARCHAR(256) NOT NULL
+);
