a3c2952ab7b1f4a91143ea9f43e98b9e5fc354e4
Axel <Axel.palombo.ap@gmail.com>
2024-11-01 20:20:26 +0000

    9th exercise

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Select ID and Name from Cities and State_Name with () to resolve and then look for
 -- Then we sort it
-SELECT cities.id, cities.name, states.name AS state_name FROM cities
+SELECT cities.id, cities.name, states.name AS name FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
