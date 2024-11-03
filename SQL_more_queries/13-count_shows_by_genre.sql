8b8c0c17e90a84a4f754e4fed36fda512898fe76
Axel <Axel.palombo.ap@gmail.com>
2024-11-03 18:57:44 +0000

    12 and 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,10 @@
 -- JOIN + HAVING
 -- TABLES
-SELECT genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY genres.name
+GROUP BY tv_genres.name
 HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
+
